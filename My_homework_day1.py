df2ac0b7c7d88c1ce1afd104fb9339d93b0e4c62
Vamir8429 <valerija8429@hotmail.com>
2022-09-06 21:28:07 +0300

    File

---
+++ My_homework_day1.py
@@ -0,0 +1,17 @@
+# 1. Print your name 5 times 
+print("Valerija "*5)
+
+# 2. print Bananananana using "Ba" once and "na" 5 times
+print("Ba"+"na"*5)
+
+# 3. print the number of seconds in a year calculated 
+# from months, days, minutes, seconds
+print(60*60*24*31)
+print(60*60*24*365)
+
+# 4. how much a googol is that is 10 to the power of the 100th
+print(10**100)
+
+# 5.how much will your 1000 Euro investment 
+# be worth after 12 years of 6% yearly interest?
+print(1000*1.06**12)
