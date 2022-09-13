cac4a409cff7e6272e23b17695762737414c31b7
Vamir8429 <valerija8429@hotmail.com>
2022-09-13 21:47:35 +0300

    final save

--- Day4_Exercise2.py
+++ Day4_Exercise2.py
@@ -12,3 +12,22 @@ for i in range(1, height+1):
     print("*" * i)
 i = i + 1
 print("Here you go")
+
+#correct solution:
+
+c_symbol = "*"
+height = 0
+while True:
+    a = input("Enter the height of the tree (positive number) or q for exit: ")
+    if a == "q":
+        break
+
+    try:
+        height = int(a)
+    except ValueError:
+        print("Please enter a number")
+        continue
+
+    for i in range(height):
+        # print(c_symbol * (2 * i - 1))
+        print(' ' * (height - i - 1) + c_symbol * (2 * i + 1))
