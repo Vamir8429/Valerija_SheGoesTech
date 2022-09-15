b8dfec3d9a73c2ae06993756defbf65bf6a3baf3
Vamir8429 <valerija8429@hotmail.com>
2022-09-15 22:05:15 +0300

    Day5 final and homework ex

--- Day5_Exercises.py
+++ Day5_Exercises.py
@@ -60,3 +60,30 @@ print(text_hided)
 
 player_text_2 = input("Second player, please enter a symbol:")
 print(player_text_2)
+
+# no idea what to do next 
+
+#Valdis solution: 
+
+word = input("First player, enter the text: ")
+# word = "Kartupeļu lauks"
+
+#guessed = "".join("*" for char in word) # downside is that this will include the empty space
+guessed = "*"*len(word) # same as above
+letter = " "  # to add back the spaces before starting
+#
+while not letter == "0":
+    # for i in range(len(word)):
+    for i, c in enumerate(word): # more Pythonic
+        # if word[i].lower() in letter.lower():  # guesses are not case sensitive
+        if c.lower() in letter.lower():  # guesses are not case sensitive
+            guessed = guessed[:i] + word[i] + guessed[i + 1:]
+
+    if guessed.find("*") == -1:
+        print("Good job!")
+        break
+
+    print(guessed)
+    letter = input("Player 2: Guess a letter (or input 0 to give up): ")
+
+print(f"The word is: {word}")
