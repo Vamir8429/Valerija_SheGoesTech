ab1d4eb4284225e37db9b021b6638b95d9d2b56b
Vamir8429 <valerija8429@hotmail.com>
2022-09-27 22:17:24 +0300

    Day 9 Final

--- Day9/Day9_Exercise3.py
+++ Day9/Day9_Exercise3.py
@@ -2,18 +2,13 @@
 
 # Write a function is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz')
 
-# that returns True when the text parameter contains all the 
-# letters passed in an alphabet.
+# that returns True when the text parameter contains all the letters passed in an alphabet.
 # We return False otherwise
 
 
+# pangram - sentence, word string containing all letters of the alphabet - https://en.wikipedia.org/wiki/Pangram
 
-# pangram - sentence, word string containing 
-# all letters of the alphabet - https://en.wikipedia.org/wiki/Pangram
-
-# We ignore spaces and believe that uppercase is as valid as lowercase, 
-# i. here A and a -> a
-
+# We ignore spaces and believe that uppercase is as valid as lowercase, i. here A and a -> a
 
 # print(is_pangram("The five boxing wizards jump quickly")) -> True
 # print(is_pangram("Not a pangram")) -> False
@@ -29,8 +24,26 @@
 # some languages have perfect pangrams, some do not
 # perfect pangram - a pangram that uses every letter of the alphabet at just once
 
-def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
-    return set(alphabet) <= set(text.lower())
+# def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
+#     text_set = set(text.lower())
+#     abc_set = set(alphabet)
+#     return abc_set.intersection(text_set) == abc_set
 
-print(is_pangram("The five boxing wizards jump quickly")) #True
-print(is_pangram("Not a pangram")) #False
+# def is_pangram(text, alphabet='abcdefghijklmnopqrstuvwxyz'):
+#     spl_text = set(text.lower())
+#     spl_alph = set(alphabet)
+#     # return spl_alph.issubset(spl_text)
+#     return spl_alph <= spl_text # same as above
+
+# one line function
+def is_pangram(text, alphabet="abcdefghijklmnopqrstuvwxyz"):
+        return set(alphabet).issubset(set(text))
+
+print(is_pangram("The five boxing wizards jump quickly"))
+print(is_pangram("Not a pangram"))
+
+a_lt="aąbcčdeęėfghiįyjklmnoprsštuųūvzž"
+print(is_pangram("Įlinkdama fechtuotojo špaga sublykčiojusi pragręžė apvalų arbūzą", alphabet=a_lt)) #true
+# translate Įlinkdama fechtuotojo špaga sublykčiojusi pragręžė apvalų arbūzą to English
+# https://translate.google.com/?sl=lt&tl=en&text=Įlinkdama%20fechtuotojo%20špaga%20sublykčiojusi%20pragręžė%20apvalų%20arbūzą&op=translate
+
