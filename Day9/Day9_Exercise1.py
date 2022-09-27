1cb38d4792f7106f56529beb1a74d7f4340ab28d
Vamir8429 <valerija8429@hotmail.com>
2022-09-27 20:36:26 +0300

    Exercise1

--- Day9/Day9_Exercise1.py
+++ Day9/Day9_Exercise1.py
@@ -10,3 +10,13 @@
 
 # the incoming sequence can be a tuple or a list with numeric values.
 
+def get_min_avg_max(sequence):
+    for item in sequence:
+        max_value = max(sequence)
+        min_value = min(sequence)
+        mean_value = int(sum(sequence)/len(sequence))
+        my_tuple = (min_value, mean_value, max_value)
+        return my_tuple
+
+print(get_min_avg_max([0,10,1,9]))
+print(get_min_avg_max([2,4,6.8]))
