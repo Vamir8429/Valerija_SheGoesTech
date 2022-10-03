59069b4a223556ad51dfacfc75e8e5a22ca461c4
Vamir8429 <valerija8429@hotmail.com>
2022-10-03 21:21:01 +0300

    All exercises

--- Day11/Day11_Exercise3.py
+++ Day11/Day11_Exercise3.py
@@ -1,1 +1,6 @@
 # 3. create a new deck in a different .py file from where Deck() is located, that means use import !
+
+import Day11_Exercise2
+
+new_deck = Day11_Exercise2.get_shuffled_cards()
+print(new_deck)
