492b5b257d8b267140ad4901a9058ea100144122
Vamir8429 <valerija8429@hotmail.com>
2022-10-03 20:25:26 +0300

    Classwork

--- Day11/Day11_Exercise1.py
+++ Day11/Day11_Exercise1.py
@@ -11,3 +11,20 @@
 # you can use a loop or use something from the library
 
 # BONUS: Something can be useful from here: https://docs.python.org/3/library/itertools.html
+
+import random
+
+def get_shuffled_cards():
+    my_values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+    my_cards = ["diamonds ♦", "hearts ♥", "spades ♠", "clubs ♣"]
+    cards = [(value, suit) for value in my_values for suit in my_cards]
+
+    random.shuffle(cards)
+    return cards
+
+if __name__ == '__main__':
+    cards = get_shuffled_cards()
+    print(cards[:5])
+
+#OUTPUT:
+#[(7, 'spades ♠'), (2, 'diamonds ♦'), (10, 'spades ♠'), (3, 'clubs ♣'), (10, 'clubs ♣')]
