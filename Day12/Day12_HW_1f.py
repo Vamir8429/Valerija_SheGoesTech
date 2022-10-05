94b2894fa31e1573816df111de60c1f6f3ef9821
Vamir8429 <valerija8429@hotmail.com>
2022-10-05 15:31:23 +0300

    HW ex 1f

--- Day12/Day12_HW_1f.py
+++ Day12/Day12_HW_1f.py
@@ -1,13 +1,14 @@
 from collections import Counter
+from pathlib import Path
 
-def get_word_usage(srcpath, destpath):
-    with open(srcpath, encoding="utf-8") as fin, open(destpath, mode="w", encoding="utf-8") as f:
+def get_word_usage(srcpath, destpath, encoding="utf-8"):
+    with open(srcpath, encoding=encoding) as fin, open(destpath, mode="w", encoding=encoding) as fon:
         text = fin.read()
-    word_list = text.split()
-    word_count = Counter(word_list)
-    result = word_count.most_common(3)
-    f.write(result)
-    print(result)
+        word_list = text.split()
+        word_count = Counter(word_list)
+        result = word_count.most_common(3)
+        for x in result:
+            fon.write(str(x))
 
 srcpath='Day12/sherlock_holmes_adventures.txt'
 destpath='Day12/count_sherlock.txt'
