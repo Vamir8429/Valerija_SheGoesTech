850a105cf539162dc2b7ba88ce1cbe324cba3201
Vamir8429 <valerija8429@hotmail.com>
2022-10-10 09:51:39 +0300

    HW

--- Day13/Day13_Homework.py
+++ Day13/Day13_Homework.py
@@ -5,31 +5,16 @@ import random
 from collections import Counter
 import matplotlib.pyplot as plt
 
-#    for _ in range(rolls_count):
-#        out.append(dice_roll(dice_count))
-#    return out
-
 # create a function that returns a list of random dice throws
 # it should have two parameters
 # - number of dice in each throw, default 4
 # - number of throws, default 100
 
-def single_throw():
-    return random.randint(1,6)
+def dice_throw(dice, throw):
+    dice_list = [random.randint(1, 4) for _ in range(0,throw)]
+    print(dice_list)
 
-def dice_throw(dice_count):
-    out = 0
-    for _ in range(dice_count):
-        out += single_throw()
-    return out
-
-def dice_rolls(dice_count, throws_count):
-    my_list = []
-    for _ in range(throws_count):
-        my_list.append(dice_throw(dice_count))
-    print(my_list)
-
-dice_rolls(3,20)
+dice_throw(3,10) #[3, 2, 1, 3, 3, 2, 1, 2, 3, 1]
 
 # even better would be a another function to which you give the list of dice throws and it 
 # plots the histogram
@@ -40,6 +25,19 @@ dice_rolls(3,20)
 
 # use the first function to create a list of 1000 dice throws with 6 dice in each throw
 # pass the result to 2nd function to plot the histogram and save it to a file
+
+def dice_throw2(dice, throw):
+    dice_list = [random.randint(1, 4) for _ in range(0,throw)]
+    print(dice_list)
+    #plot a histogram of the list
+    plt.hist(dice_list,bins=range(dice+1,throw+1))
+    plt.xlabel('Throw')
+    plt.ylabel('Dice')
+    plt.title('Histogram of Dice Throws')
+    plt.show()
+
+dice_throw2(3,10)
+
 # BONUS: file name should have current date and time in it
 
 # ideally you would then run them from main guard
