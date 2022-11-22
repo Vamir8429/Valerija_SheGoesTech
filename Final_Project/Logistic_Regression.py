cf1147a64443c3c18bb7c4d07e268583ddea3cd1
Vamir8429 <valerija8429@hotmail.com>
2022-11-22 19:06:18 +0200

    slight amendment on imports

--- Final_Project/Logistic_Regression.py
+++ Final_Project/Logistic_Regression.py
@@ -1,18 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
 import json
  
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from xgboost import XGBClassifier
 from sklearn import metrics
- 
-import warnings
-warnings.filterwarnings('ignore')
 
 #function, assuming dataset is processed according above workbook
 
